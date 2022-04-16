507f7ab88f8635d3a3e560e305dad119f8de5687
andrejaccedo <89917860+andrejaccedo@users.noreply.github.com>
2022-04-16 17:05:40 +0200

    Hubspot workflow default template

---
+++ src/workflow.js
@@ -0,0 +1,30 @@
+const axiosDefault = require("axios");
+
+// ? Define axios instance
+// const axios = axiosDefault.create({
+//   baseURL: URL,
+// });
+
+exports.main = async (event, callback) => {
+  // ? Export inputFields
+  // const { INPUT_FIELD } = event.inputFields;
+
+  // ? Get ENV Variables
+  // const { ENV_VARIABLE } = process.env;
+
+  try {
+    // ? Have Success Callback function
+    // callback({
+    //   outputFields: {
+    //     error: false,
+    //   },
+    // });
+  } catch (err) {
+    // ? Have Error Callback function
+    // callback({
+    //   outputFields: {
+    //     error: true,
+    //   },
+    // });
+  }
+};
